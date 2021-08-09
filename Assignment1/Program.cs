bb29c094f9cb640c6b44387a008675ca5f04f2b7
Thanh Tung <tung09733@gmail.com>
2021-08-09 16:33:30 +0700

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Username :");
+            string userName = Console.ReadLine();
+            
+            Console.WriteLine("Username is : " + userName);
+
+            
+            Console.WriteLine("Choose What To Do : ");
+            Console.WriteLine("1. Adminitrative changes");
+            Console.WriteLine("2. List Of Branches ");
+
+            int ten;
+            ten = Int32.Parse(Console.ReadLine());
+
+            switch (ten)
+            {
+                case 1:
+                    Console.WriteLine("At the start of the 2002-2003 school year, co-principals Boaz Kita'in and Diana Shaloufi-Rizek were replaced by new principals after a careful selection process made during the previous school year and preparation made during the summer.");
+                    break;
+                case 2:
+                    Console.WriteLine("APAX LEADERS - Luong Yen Tang 3, Sun Grand Ancora City, 3 Lương Yên · In Sun Grand City Ancora Residence · 1900 636056Closed ⋅ Opens 10AM Tue");
+                    Console.WriteLine("PoPoDoo Vietnam Education JSCNo reviews · Language school --- 468 Thụy Khuê · 024 6251 4444");
+                    break;
+            }
+        }
+    }
+}
